59982083a83f42e9062a56b534cf05be576d6325
Aleksey Kiyashkin <akiyashkin@gmail.com>
2023-09-13 09:35:41 +0000

    kubernetes deployment success

---
+++ backend/bootstrap.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python manage.py migrate
+python manage.py collectstatic --noinput
+cp -r /app/collected_static/. /backend_static/static/
+gunicorn --bind 0.0.0.0:9000 kittygram_backend.wsgi
